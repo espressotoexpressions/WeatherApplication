e2e93dd688e9f6f2ec97627e39c88c4f6f491040
espressotoexpressions <keannevillar@gmail.com>
2024-12-09 16:37:45 -0800

    updated script

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,12 @@
 import { APIKEY } from './environment.js'; 
 
 //assigned values are for testing purposes only
-let latitude = 44.34;
-let longitude= 10.99;
-let locationName = "Stockton,CA";
+let latitude =44.34 ;
+let longitude =10.99;
+let locationName = "Stockton,CA,US";
 
 function getCoordinatesByLocationName(locationName){
-    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${locationName}&limit=5&appid=${APIKEY}`)
+    return fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${locationName}&limit=5&appid=${APIKEY}`)
     .then((response) => {
         return response.json();
     })
@@ -15,25 +15,26 @@ function getCoordinatesByLocationName(locationName){
         return (data);
     })
 }
+function getCurrentWeatherData (latitude,longitude) {
+ 
+    return         fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${APIKEY}`)
+        .then((response) => {
+            return response.json()
+        })
+        .then((data) => {
+            console.log(`CURRENT`+JSON.stringify(data));
+            return (data);
+        })
+        
 
-
-function getCurrentWeatherData (latitude, longitude) {
-
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${APIKEY}`)
-    .then((response) => {
-        return response.json()
-    })
-    .then((data) => {
-        console.log(`CURRENT`+JSON.stringify(data));
-        return (data);
-    })
+   
 }
 
- getCurrentWeatherData(latitude,longitude);
+
 
  function get5DayForecastData (latitude, longitude) {
 
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${APIKEY}`)
+    return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${APIKEY}`)
     .then((response) => {
         return response.json()
     })
@@ -44,7 +45,10 @@ function getCurrentWeatherData (latitude, longitude) {
 }
 
 
+let arrCoordinates = getCoordinatesByLocationName(locationName);
+getCurrentWeatherData(latitude,longitude);
 get5DayForecastData(latitude,longitude);
+
 
 
 
