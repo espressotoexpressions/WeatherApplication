d22af2dc23407b01364031bb4a5c8bb125023868
espressotoexpressions <keannevillar@gmail.com>
2024-12-11 16:13:55 -0800

    added populating 5 day forecast data and icon images

--- scripts/app.js
+++ scripts/app.js
@@ -12,6 +12,8 @@ let currentWeather = document.getElementById("currentWeather");
 let currentTemp = document.getElementById("currentTemp");
 let currentMax= document.getElementById("currentMax");
 let currentMin = document. getElementById("currentMin");
+let currentIcon = document.getElementById("currentIcon");
+// forecast day1 
 
 
 
@@ -20,43 +22,87 @@ async function getCoordinatesByLocationName(locationName){
     const promise = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${locationName}&limit=5&appid=${APIKEY}`);
     const data= await promise.json(); // transforms it to a json format
     console.log(`DATA get coordinates` +JSON.stringify(data));
-    let cityName =`${data[0].name}, ${data[0].state}, ${ data[0].country}`;
-    currentCityName.innerText=cityName;
-    getCurrentWeatherData(data);
-    get5DayForecastData(data);
-    return data;
+
+    
+    if (data.length>0)
+        {
+            let cityName =`${data[0].name}, ${data[0].state}, ${ data[0].country}`;
+            currentCityName.innerText=cityName;
+            getCurrentWeatherData(data);
+            get5DayForecastData(data);
+            
+        }
+        else {
+            alert("No city found.");
+        }
+    //return data;
   }
 
-  async function getCurrentWeatherData(coordinatesData){
-   const promise = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${coordinatesData[0].lat}&lon=${coordinatesData[0].lon}&appid=${APIKEY}&units=imperial`); //for Farenheit units
-   const data= await promise.json(); // transforms it to a json format
+async function getCurrentWeatherData(coordinatesData){
+const promise = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${coordinatesData[0].lat}&lon=${coordinatesData[0].lon}&appid=${APIKEY}&units=imperial`); //for Farenheit units
+const data= await promise.json(); // transforms it to a json format
 
-   // Access the Unix timestamp from the 'dt' field
+//icon
+let iconCode = data.weather[0].icon; // accesss first weather condition as it is the primary if multiple weather condition is returned
+console.log(iconCode+ "ICONCODE for current");
+
+currentIcon.src = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+console.log(currentIcon.src);
+// Access the Unix timestamp from the 'dt' field
 const timestamp = data.dt;
+
 // Convert the timestamp to a JavaScript Date object
 const date = new Date(timestamp * 1000); // Multiply by 1000 to convert from seconds to milliseconds
+
 // Display the formatted date
 currentDate.innerText=date.toLocaleString(); // Output: A human-readable date
 
-   currentTemp.innerText=data.weather[0].description;
-//    let fTemp= (((data.main.temp) - 273.15) * 9/5 + 32).toFixed(2);// converts kelvin to farenheight
+currentTemp.innerText=data.weather[0].description;
 
-   currentWeather.innerText=`${data.main.temp} °F`;
+   currentWeather.innerText=`${data.main.temp}°F`;
    currentMax.innerText=`${data.main.temp_max}°F`;
    currentMin.innerText=`${data.main.temp_min}°F`;
 
    console.log("CURR WEATHER"+JSON.stringify(data.weather[0].description));
    console.log(`CURRENT DATA`+ JSON.stringify(data));
 
-//    return data;
  }
 
  async function get5DayForecastData(coordinatesData){
   
-   const promise = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${coordinatesData[0].lat}&lon=${coordinatesData[0].lon}&appid=${APIKEY}`);
+   const promise = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${coordinatesData[0].lat}&lon=${coordinatesData[0].lon}&appid=${APIKEY}&units=imperial`);
    const data= await promise.json(); // transforms it to a json format
    console.log(`forecast DATA`+ JSON.stringify(data));
-   return data;
+
+   //forecast data population in UI
+   for (let i=0; i<5;i++)// because we only want the top 5 entries of the returned json data
+    {
+
+        let varDayName =   `forecast${i}Day`;
+        let iconName = `forecast${i}Icon`;
+        let forecastWeatherName =   `forecast${i}weather`;
+        let forecastMaxName = `forecast${i}max`;
+        let forecastMinName = `forecast${i}min`; 
+        
+        let forecastDayElement = document.getElementById(varDayName.toString());
+        let forecastIconElement = document.getElementById(iconName.toString());
+        let forecastWeatherElement = document.getElementById(forecastWeatherName.toString());
+        let forecastMaxElement = document.getElementById(forecastMaxName.toString());
+        let forecastMinElement = document.getElementById(forecastMinName.toString());
+        console.log(varDayName+ iconName + forecastWeatherName + forecastMaxName +forecastMinName);
+        console.log("VARIABLE" + data.list[i].main.temp);
+        forecastDayElement.innerText = data.list[i].dt_txt;
+        
+        //icon mapping from weather api
+        let iconCode = data.list[i].weather[0].icon; // accesss first weather condition as it is the primary if multiple weather condition is returned
+        console.log(iconCode+ "ICONCODE"+i);
+        forecastIconElement.src = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+        
+        forecastWeatherElement.innerText =data.list[i].main.temp;
+        forecastMaxElement.innerText=`${data.list[i].main.temp_max}°F`;
+        forecastMinElement.innerText=`${data.list[i].main.temp_min}°F`;
+    }
+
  }
  
 
@@ -65,3 +111,4 @@ searchIcon.addEventListener('click',function(){
     let locationName = searchInput.value ;
     getCoordinatesByLocationName(locationName);
 })
+
